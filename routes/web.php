161a608faf9b49569142abf2ab579d4e89be601a
DeiverJC <deiver-jc@live.com>
2022-03-06 22:00:16 -0500

    Update welcome view

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return "Hola mundo!";
-    //return view('welcome');
+    return view('welcome');
 });
 
 Route::get('/products', [ProductController::class, 'index']);
